fee7df3bfc3dd939c0a8479b2d37dee7d001dcc2
YAGI0423 <9945735@naver.com>
2022-10-18 16:29:18 +0900

    write 'forwardProb'

--- forwardProb.py
+++ forwardProb.py
@@ -1,3 +1,24 @@
+from abc import *
+
+class Layer:
+    @abstractmethod
+    def forwardProp(self):
+        pass
+
+    @abstractmethod
+    def backProb(self):
+        pass
+
+class FALayer(Layer):
+    def __init__(self):
+        pass
+
+    def forwardProp(self):
+        pass
+
+    def backProb(self):
+        pass
+
 
 if __name__ == '__main__':
-    print('hi')
+    faLayer = FALayer()
