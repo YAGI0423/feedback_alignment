fdea75bdf68df3a66b45c7ed4b27e26256df8fd6
YAGI0423 <9945735@naver.com>
2022-10-19 02:51:07 +0900

    write Loss.py

--- layer.py
+++ layer.py
@@ -44,8 +44,19 @@ class BPLayer(Layer):
     def backProb(self, dy):
         db = dy
         dx = np.dot(dy, self.W.T)
+
+        #<Personal Method>
+        '''
+        보편적인 방법에 해당하는 『Affine Method』의 경우,
+        배치 별 가중치 미분 값을 합하여 출력한다.
+        『Personal Methd』의 경우 배치에 따른 가중치를 그대로 출력한다.
+        '''
+        dW = np.multiply(dy, self._rec_x)
+        return dx, dW, db
+
+        #<Affine Method>
         xT = np.transpose(self._rec_x)
-        dW = xT * dy
+        dW = np.dot(xT, dy)
         return dx, dW, db
 
 class Sigmoid(Layer):
@@ -86,8 +97,8 @@ if __name__ == '__main__':
     print('W: ', faLayer.W, end='\n\n')
     print('b: ', faLayer.b, end='\n\n')
 
-    x = [[0, 0], [0, 1]]
-    y = [[0], [1]]
+    x = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    y = [[0], [1], [1], [1]]
 
     print('x: ', x, end='\n\n')
     print('y: ', y, end='\n\n')
@@ -113,6 +124,3 @@ if __name__ == '__main__':
     print('dx: ', dx, end='\n\n')
     print('dW: ', dW, end='\n\n')
     print('db: ', db, end='\n\n')
-
-    
-    
