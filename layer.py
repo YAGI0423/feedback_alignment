0b3d43fd83ebb52af1cc924de290212a27a67cc6
YAGI0423 <9945735@naver.com>
2022-10-19 22:00:12 +0900

    write 'layer.py'

--- layer.py
+++ layer.py
@@ -24,18 +24,10 @@ class BPLayer(Layer):
         self.W = np.random.rand(input_shape, units)
         self.b = np.random.rand(units)
 
-        self.W = np.array(
-            [
-                [0.34995712],
-                [0.260819],
-            ]
-        )
-        self.b = np.array([0.32036945])
-
         self._rec_x = None
 
     def forwardProp(self, x):
-        xW = np.dot(x, self.W)
+        xW = np.matmul(x, self.W)
         h = xW + self.b
 
         self._rec_x = x
@@ -79,13 +71,23 @@ class MSE(LossFunction):
 
     def forwardProp(self, y_hat, y):
         err = np.subtract(y_hat, y)
-        loss = 0.5 * err**2
+        loss = np.square(err)
+        loss = np.mean(loss)
 
         self._rec_err = err
         return loss
 
     def backProb(self):
-        return self._rec_err
+        batch_by_class = np.prod(self._rec_err.shape) #batch size * class num
+        dloss = (2 / batch_by_class) * self._rec_err
+        
+        #<Batch x class로 나누는 이유에 대해>
+        '''
+        손실함수 단계에서 미리 미분값을 배치 크기로 나누면,
+        이후 Affine 계층의 ∂L/∂W 값을 matmul(Δy, Δx.T) 만으로 배치 크기를 고려하여 구할 수있다.
+        해당 연산이 없는 경우, 동일한 미분값을 얻기위해서는 각 ΔW를 배치 크기로 나누어주어야 하는 번거로움이 있다. 
+        '''
+        return dloss
 
 
 
