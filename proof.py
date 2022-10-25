930bf99859d0e81f18c04985dbe7f22537f845ab
YAGI0423 <9945735@naver.com>
2022-10-26 00:12:22 +0900

    write 'proof.py'

--- proof.py
+++ proof.py
@@ -6,7 +6,6 @@ from ann import optimizers
 
 from mnist_dataset import datasetLoader
 
-import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
@@ -15,7 +14,7 @@ if __name__ == '__main__':
     def get_model():
         inputs = layers.InputLayer(shape=(784, ))
         out = layers.BPLayer(input_shape=784, units=1000, weight_init=Inintializers.He)(inputs)
-        out = layers.ReLU()(out)
+        out = layers.Sigmoid()(out)
 
         out = layers.BPLayer(input_shape=1000, units=10, weight_init=Inintializers.Xavier)(out)
         out = layers.Softmax()(out)
@@ -43,7 +42,7 @@ if __name__ == '__main__':
     datset = datasetLoader.Loader(is_normalize=True)
 
     train_x, train_y = datset.loadTrainDataset(batch_size=32, is_shuffle=True)
-    test_x, test_y = datset.loadTestDataset(batch_size=8, is_shuffle=False)
+    test_x, test_y = datset.loadTestDataset(batch_size=64, is_shuffle=False)
     
     model = get_model()
 
@@ -52,54 +51,7 @@ if __name__ == '__main__':
 
     losses = train_model(x=train_x, y=train_y, model=model, loss_function=lossFunction)
 
-        
-    
-    
 
     plt.plot(losses)
     plt.show()
     exit()
-
-    
-    
-
-    def get_model():
-        inputs = layers.InputLayer(shape=(2, ))
-        out = layers.BPLayer(input_shape=2, units=1, weight_init=Inintializers.Xavier)(inputs)
-        out = layers.Sigmoid()(out)
-
-        out = layers.BPLayer(input_shape=1, units=4, weight_init=Inintializers.Xavier)(out)
-        out = layers.Softmax()(out)
-
-        model = models.Model(inputs=inputs, outputs=out)
-        return model
-
-    model = get_model()
-    model.optimizer = optimizers.SGD(learning_rate=0.1)
-
-    lossFunction = lossFunctions.CrossEntropy()
-
-    x = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    y = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-
-    for _ in range(30000):
-        y_hat = model.predict(x)
-        loss = lossFunction.forwardProp(y_hat=y_hat, y=y)
-        print(loss)
-
-        dLoss = lossFunction.backProp()
-        model.update_on_batch(dLoss)
-
-    print(model.predict([[1, 1]]).round(2))
-    # print(model.predict([[1, 1]]))
-    
-    # lay = model.input_layer
-    # while lay is not None:
-    #     try:
-    #         print('name:', lay.__class__.__name__)
-    #         print('W:', lay.W)
-    #         print('b:', lay.b, end='\n\n')
-    #     except:
-    #         pass
-    #     lay = lay.childLayer
-    #     pass
