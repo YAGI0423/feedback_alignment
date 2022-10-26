a90f50871c76a47f39f53fbde88af960b37e8626
YAGI0423 <9945735@naver.com>
2022-10-27 00:23:37 +0900

    edit 'loss graph'

--- proof.py
+++ proof.py
@@ -10,8 +10,48 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
-if __name__ == '__main__':
-    def get_model():
+from abc import abstractmethod, ABCMeta
+class ModelFrame(metaclass=ABCMeta):
+    @abstractmethod
+    def create_model(self):
+        pass
+
+    def train(self, x, y):
+        batch_size = len(x)
+        dataset_iter = tqdm(zip(x, y), total=batch_size)
+
+        losses = []
+        for x, y in dataset_iter:
+            y_hat = self.model.predict(x=x)
+            loss = self.lossF.forwardProp(y_hat=y_hat, y=y)
+
+            dLoss = self.lossF.backProp()
+            self.model.update_on_batch(dLoss)
+
+            dataset_iter.set_description(f'Loss: {loss:.5f}')
+            losses.append(loss)
+        return losses
+
+    def inference(self, x, y):
+        batch_size = len(x)
+        dataset_iter = tqdm(zip(x, y), total=batch_size)
+        
+        losses = []
+        for x, y in dataset_iter:
+            y_hat = self.model.predict(x=x)
+            loss = self.lossF.forwardProp(y_hat=y_hat, y=y)
+
+            dataset_iter.set_description(f'Loss: {loss:.5f}')
+            losses.append(loss)
+        return losses
+
+class BPmodel(ModelFrame):
+    def __init__(self, optimizer, lossFunction):
+        self.model = self.create_model()
+        self.model.optimizer = optimizer
+        self.lossF = lossFunction
+
+    def create_model(self):
         inputs = layers.InputLayer(shape=(784, ))
         out = layers.BPLayer(input_shape=784, units=1000, weight_init=Inintializers.Xavier)(inputs)
         out = layers.Sigmoid()(out)
@@ -22,47 +62,17 @@ if __name__ == '__main__':
         model = models.Model(inputs=inputs, outputs=out)
         return model
 
-    def train_model(x, y, model, loss_function):
-        batch_size = len(x)
-        dataset_iter = tqdm(zip(x, y), total=batch_size)
 
-        losses = []
-        for x, y in dataset_iter:
-            y_hat = model.predict(x=x)
-            loss = loss_function.forwardProp(y_hat=y_hat, y=y)
-
-            dLoss = loss_function.backProp()
-            model.update_on_batch(dLoss)
-
-            dataset_iter.set_description(f'Loss: {loss:.5f}')
-            losses.append(loss)
-        return losses
-
-    def inference_model(x, y, model, loss_function):
-        batch_size = len(x)
-        dataset_iter = tqdm(zip(x, y), total=batch_size)
-        
-        losses = []
-        for x, y in dataset_iter:
-            y_hat = model.predict(x=x)
-            loss = loss_function.forwardProp(y_hat=y_hat, y=y)
-
-            dataset_iter.set_description(f'Loss: {loss:.5f}')
-            losses.append(loss)
-        return losses
-        
-
+if __name__ == '__main__':
     dataset = datasetLoader.Loader(is_normalize=True)
-
-    train_x, train_y = dataset.loadTrainDataset(batch_size=64, is_shuffle=True)
     test_x, test_y = dataset.loadTestDataset(batch_size=1, is_shuffle=False)
-    
-    model = get_model()
 
-    model.optimizer = optimizers.SGD(learning_rate=0.001)
+    optimizer = optimizers.SGD(learning_rate=0.001)
     lossFunction = lossFunctions.SparseCrossEntropy(class_num=10)
 
-    EPOCH = 3
+    model = BPmodel(optimizer=optimizer, lossFunction=lossFunction)
+
+    EPOCH = 10
     BATCH_SIZE = 64
 
     total_train_losses = []
@@ -71,8 +81,8 @@ if __name__ == '__main__':
         print(f'EPOCH ({e+1}/{EPOCH})')
         train_x, train_y = dataset.loadTrainDataset(batch_size=BATCH_SIZE, is_shuffle=True)
 
-        train_losses = train_model(x=train_x, y=train_y, model=model, loss_function=lossFunction)
-        test_losses = inference_model(x=test_x, y=test_y, model=model, loss_function=lossFunction)
+        train_losses = model.train(x=train_x, y=train_y)
+        test_losses = model.inference(x=test_x, y=test_y)
         print()
 
         test_loss = sum(test_losses) / len(test_losses)
@@ -81,14 +91,40 @@ if __name__ == '__main__':
         test_loss_epoch.append(test_loss)
 
 
+    def pltDefaultSetting(plt, title: str, ticks, labels):
+        title_args ={'fontsize': 13, 'fontweight': 'bold'}
+
+        plt.title(title, fontdict=title_args, loc='left', pad=10)
+
+        plt.xticks(ticks=ticks, labels=labels)
+        plt.tick_params(axis='x', direction='in')
+        plt.tick_params(axis='y', direction='in')
+
+    
+
+    plt.figure(figsize=(7, 9))
+    plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.925, wspace=0.1, hspace=0.3)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(total_train_losses, color='k', label='BP')
+
+    size_per_epoch = int(len(total_train_losses) / EPOCH) - 1
+    
+    ticks = tuple(e * size_per_epoch for e in range(0, EPOCH+1, 5))
+    labels = tuple(range(0, EPOCH+1, 5))
+
+    pltDefaultSetting(plt, title='<Loss on Train Set>', ticks=ticks, labels=labels)
 
 
-    plt.figure(figsize=(9, 3))
-    plt.subplot(1, 2, 1)
-    plt.plot(total_train_losses)
+    plt.subplot(2, 1, 2)
+    plt.plot(test_loss_epoch, color='k', label='BP')
 
-    plt.subplot(1, 2, 2)
-    plt.plot(test_loss_epoch)
+    ticks = list(range(-1, EPOCH+1, 5))
+    ticks[0] = 0
+    labels = list(range(0, EPOCH+1, 5))
+    labels[0] = 1
+
+    pltDefaultSetting(plt, title='<Loss on Test Set>', ticks=ticks, labels=labels)
 
     plt.show()
     
