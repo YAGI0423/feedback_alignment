1669a9fb49a32c82f2efb5d380a4282b26c861aa
YAGI0423 <9945735@naver.com>
2022-10-28 02:07:34 +0900

    write 'paperReproduction'

--- historyVisualizer.py
+++ historyVisualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 def __pltDefaultSetting(plt, title: str, ticks, labels):
     title_args ={'fontsize': 13, 'fontweight': 'bold'}
 
@@ -10,7 +11,7 @@ def __pltDefaultSetting(plt, title: str, ticks, labels):
     plt.tick_params(axis='y', direction='in')
 
 def visualize(train_losses: dict, test_losses: dict, epoch: int):
-    COLOR_LIST = ['#00AF00', '#000000']
+    COLOR_LIST = ['#000000', '#00AF00']
 
     plt.figure(figsize=(7, 9))
     plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.925, wspace=0.1, hspace=0.3)
@@ -19,6 +20,7 @@ def visualize(train_losses: dict, test_losses: dict, epoch: int):
     plt.subplot(2, 1, 1)
     for (name, losses), color in zip(train_losses.items(), COLOR_LIST):
         plt.plot(losses, color=color, label=name)
+    plt.legend(loc='upper right')
 
     size_per_epoch = int(len(losses) / epoch) - 1
     
@@ -33,6 +35,7 @@ def visualize(train_losses: dict, test_losses: dict, epoch: int):
     plt.subplot(2, 1, 2)
     for (name, losses), color in zip(test_losses.items(), COLOR_LIST):
         plt.plot(losses, color=color, label=name)
+    plt.legend(loc='upper right')
 
     ticks = list(range(-1, epoch+1, 5))
     ticks[0] = 0
--- paperReproduction.py
+++ paperReproduction.py
@@ -6,25 +6,15 @@ from mnist_dataset import datasetLoader
 
 import historyVisualizer
 
-if __name__ == '__main__':
-    dataset = datasetLoader.Loader(is_normalize=True)
+
+def trainModel(model, dataset, epoch: int, batch_size: int):
     test_x, test_y = dataset.loadTestDataset(batch_size=1, is_shuffle=False)
-
-    optimizer = optimizers.SGD(learning_rate=0.001)
-    lossFunction = lossFunctions.SparseCrossEntropy(class_num=10)
-
-    model = validateModels.FAmodel(optimizer=optimizer, lossFunction=lossFunction)
-
-
-
-    EPOCH = 10
-    BATCH_SIZE = 64
 
     total_train_losses = []
     total_test_losses = []
-    for e in range(EPOCH):
+    for e in range(epoch):
         print(f'EPOCH ({e+1}/{EPOCH})')
-        train_x, train_y = dataset.loadTrainDataset(batch_size=BATCH_SIZE, is_shuffle=True)
+        train_x, train_y = dataset.loadTrainDataset(batch_size=batch_size, is_shuffle=True)
 
         train_losses = model.train(x=train_x, y=train_y)
         test_losses = model.inference(x=test_x, y=test_y)
@@ -34,11 +24,29 @@ if __name__ == '__main__':
 
         total_train_losses.extend(train_losses)
         total_test_losses.append(test_loss)
+    return total_train_losses, total_test_losses
 
+
+if __name__ == '__main__':
+    EPOCH = 10
+    BATCH_SIZE = 64
+
+    dataset = datasetLoader.Loader(is_normalize=True)
+    test_x, test_y = dataset.loadTestDataset(batch_size=1, is_shuffle=False)
+
+    optimizer = optimizers.SGD(learning_rate=0.001)
+    lossFunction = lossFunctions.SparseCrossEntropy(class_num=10)
+
+    bp_model = validateModels.BPmodel(optimizer=optimizer, lossFunction=lossFunction)
+    fa_model = validateModels.FAmodel(optimizer=optimizer, lossFunction=lossFunction)
+
+
+    bp_train_his, bp_test_his = trainModel(model=bp_model, dataset=dataset, epoch=EPOCH, batch_size=BATCH_SIZE)
+    fa_train_his, fa_test_his = trainModel(model=fa_model, dataset=dataset, epoch=EPOCH, batch_size=BATCH_SIZE)
     
     historyVisualizer.visualize(
-        train_losses={'BP': total_train_losses},
-        test_losses={'BP': total_test_losses},
+        train_losses={'BP': bp_train_his, 'FA': fa_train_his},
+        test_losses={'BP': bp_test_his, 'FA': fa_test_his},
         epoch=EPOCH
     )
 
